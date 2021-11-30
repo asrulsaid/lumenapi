f904cf8e750258823db76e2cb3f7c06a427b92b4
asrul said <asrulsaid313@gmail.com>
2021-11-30 11:56:00 +0800

    init

---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('/feedbacks', 'FeedbackController@getFeedbackAll');
+$router->get('/feedbacks/{id}', 'FeedbackController@getFeedbackbyId');
+$router->delete('/feedbacks/{id}', 'FeedbackController@deleteFeedback');
+$router->get('/feedbacks/users/{id_user}', 'FeedbackController@searchUser');
+$router->get('/feedbacks/date/{date}', 'FeedbackController@searchDate');
+$router->get('/feedbacks/search', 'FeedbackController@search');
